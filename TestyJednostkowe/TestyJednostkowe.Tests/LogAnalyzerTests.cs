297b6bf0e5d7fab603de7c2d4f905509d6127c95
Grzegorz Prawdzik <grzegorz.prawdzik@qtravel.pl>
2016-12-12 20:15:03 +0100

    początek

---
+++ TestyJednostkowe/TestyJednostkowe.Tests/LogAnalyzerTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestyJednostkowe;
+
+namespace TestyJednostkowe.Tests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidFileName_BadExtension_ReturnsFalse()
+        {
+            LogAnalyzer an = new LogAnalyzer();
+            bool results = an.IsValidLogFileName("hsdjfhf.foo");
+            Assert.False(results);
+        }
+        
+        [Test]
+        public void IsValidFileName_GoodExtensionUppercase_ReturnsTrue()
+        {
+            LogAnalyzer an = new LogAnalyzer();
+            bool results = an.IsValidLogFileName("hsdjfhf.SLF");
+            Assert.True(results);
+        }
+
+        [Test]
+        public void IsValidFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            LogAnalyzer an = new LogAnalyzer();
+            bool results = an.IsValidLogFileName("hsdjfhf.slf");
+            Assert.True(results);
+        }
+    }
+}
---
+++ TestyJednostkowe/TestyJednostkowe/LogAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestyJednostkowe
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            return fileName.EndsWith(".slf", StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
