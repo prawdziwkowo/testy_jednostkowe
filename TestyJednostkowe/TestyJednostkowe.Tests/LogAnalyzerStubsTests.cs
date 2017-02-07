95f25f6389fa70d26bcad0a8acea65cebcbc3972
Grzegorz Prawdzik <g.prawdzik@gmail.com>
2017-02-07 22:11:09 +0100

    Namiastki

---
+++ TestyJednostkowe/TestyJednostkowe.Tests/LogAnalyzerStubsTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestyJednostkowe;
+
+namespace TestyJednostkowe.Tests
+{
+    [TestFixture]
+    class LogAnalyzerStubsTests
+    {
+        [Test]
+        public void IsValidFileName_nameSupportedExtention_ReturnsTrue()
+        {
+            FakeExtentionManager fakeManger = new FakeExtentionManager();
+            fakeManger.willBeValid = true;
+
+            LogAnalyzerStubs log = new LogAnalyzerStubs(fakeManger);
+            Assert.True(log.isValidLogFileName("plik.ext"));
+
+        }
+        
+        [Test]
+        public void IsValidFileName_ExtManagerThrowsException_Throws()
+        {
+            string exceptionMessage = "Wyjątek jakiś normalnie o tej porze";
+            FakeExtentionManager fakeManger = new FakeExtentionManager();
+            fakeManger.willThrow = new Exception("Wyjątek jakiś normalnie o tej porze");
+            LogAnalyzerStubs log = new LogAnalyzerStubs(fakeManger);
+            var ex = Assert.Throws<Exception>(() => log.isValidLogFileName("anything.anything")); //to jest wyrażenie lambda, po lewej argumenty po prawej tresc
+            StringAssert.Contains(exceptionMessage, ex.Message);
+        }
+    }
+
+    internal class FakeExtentionManager : IExtentionManager
+    {
+        public bool willBeValid = false;
+        public Exception willThrow = null;
+        public bool isValid(string fileName)
+        {
+            if (willThrow != null)
+            {
+                throw willThrow;
+            }
+            return willBeValid;
+        }
+    }
+}
---
+++ TestyJednostkowe/TestyJednostkowe/LogAnalyzerStubs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestyJednostkowe
+{
+    public class LogAnalyzerStubs
+    {
+        private IExtentionManager manager;
+
+        public LogAnalyzerStubs(IExtentionManager mgr)
+        {
+            manager = mgr;
+        }
+        public bool isValidLogFileName(string fileName)
+        {
+            return manager.isValid(fileName);
+        }
+    }
+
+    public interface IExtentionManager
+    {
+        bool isValid(string fileName);
+    }
+
+    public class FileExtentionManager : IExtentionManager
+    {
+        public bool isValid(string fileName)
+        {
+            return true;
+        }
+    }
+
+    //namiasta
+    public class AlwaysValidFakeExtentionManager : IExtentionManager
+    {
+        public bool isValid(string fileName)
+        {
+            return true;
+        }
+    }
+}
